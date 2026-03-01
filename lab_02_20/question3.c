376607db6d036b94494790b1602aeb7698be9ee2
ANKIT KUMAR <ak9005435@gmail.com>
2026-03-01 05:15:51 +0000

    update code

--- lab_02_20/question3.c
+++ lab_02_20/question3.c
@@ -2,39 +2,34 @@
 
 int main() {
     int n = 5;
+    int bt[] = {5, 7, 6, 8, 5};
+    int io[] = {2, 2, 3, 1, 2};
+    int at[] = {0, 0, 0, 0, 0};
+    
+    int tat[5], wt[5], ct[5];
+    int total_tat = 0, total_wt = 0;
+    int time = 0;
 
-        int AT[5] = {0, 0, 0, 0, 0};
-            int BT[5] = {5, 7, 6, 8, 5};
-                int IO[5] = {2, 2, 3, 1, 2};
+    for(int i = 0; i < n; i++) {
+        int total_time = bt[i] + io[i];
+        
+        time += total_time;     // completion time
+        ct[i] = time;
+        
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - total_time;
 
-                    int CT[5], TAT[5], WT[5];
-                        int totalTAT = 0, totalWT = 0;
+        total_tat += tat[i];
+        total_wt += wt[i];
+    }
 
-                            int currentTime = 0;
+    printf("Process\tCT\tTAT\tWT\n");
+    for(int i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\n", i, ct[i], tat[i], wt[i]);
+    }
 
-                                printf("Process\tAT\tBT\tIO\tCT\tTAT\tWT\n");
+    printf("\nAverage Turnaround Time = %.2f", (float)total_tat/n);
+    printf("\nAverage Waiting Time = %.2f", (float)total_wt/n);
 
-                                    for(int i = 0; i < n; i++) {
-
-                                            // Completion Time
-                                                    currentTime += BT[i] + IO[i];
-                                                            CT[i] = currentTime;
-
-                                                                    // Turnaround Time
-                                                                            TAT[i] = CT[i] - AT[i];
-
-                                                                                    // Waiting Time
-                                                                                            WT[i] = TAT[i] - (BT[i] + IO[i]);
-
-                                                                                                    totalTAT += TAT[i];
-                                                                                                            totalWT += WT[i];
-
-                                                                                                                    printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
-                                                                                                                                   i, AT[i], BT[i], IO[i], CT[i], TAT[i], WT[i]);
-                                                                                                                                       }
-
-                                                                                                                                           printf("\nAverage Turnaround Time = %.2f", (float)totalTAT / n);
-                                                                                                                                               printf("\nAverage Waiting Time = %.2f\n", (float)totalWT / n);
-
-                                                                                                                                                   return 0;
-                                                                                                                                                   }
+    return 0;
+}
